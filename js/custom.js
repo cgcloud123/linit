6a47acc597f3d77e3e24a586edbb03d091e0d526
cgcloud123 <109736846+cgcloud123@users.noreply.github.com>
2022-08-16 18:07:09 +0530

    commit

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".team_button1").click(function(){
+      $(".info_team1").toggle();
+    });
+  });
+
+  $(document).ready(function(){
+    $(".team_button2").click(function(){
+      $(".info_team2").toggle(); 
+    });
+    
+  });
+  
+
+  $(document).ready(function(){
+    $(".team_button3").click(function(){
+      $(".info_team3").toggle();
+    });
+  });
+
+  $(document).ready(function(){
+    $(".team_button4").click(function(){
+      $(".info_team4").toggle();
+    });
+  });
